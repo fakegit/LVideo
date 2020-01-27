4fcb8299ff1272cd9efc634c65a5ccf56b544b04
joelYing <635328166@qq.com>
2020-01-27 19:51:20 +0800

    deploy

--- LVideo/settings.py
+++ LVideo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+_ld&jcj7l%*y$gi_wxj-y5dnldwt50%gj(*-g=_b)&lq5fv=x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['busishu', 'localhost', '49.235.203.45', '127.0.0.1']
 
 
 # Application definition
